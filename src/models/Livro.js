6aafd7096bedec33c58c5655683d71090a8c0136
Bruno <bruno765rez@gmail.com>
2025-09-08 11:41:06 -0300

    Finalizar CRUD para Autores

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,7 +4,7 @@ import { autorSchema } from "./Autor.js";
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     titulo: { type: String, required: true },
-    editora: { type: String, required: true },
+    editora: { type: String },
     preco: { type: Number },
     paginas: { type: Number },
     autor: autorSchema,
